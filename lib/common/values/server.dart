34b10637e38500afef73e7a81efc985470a4d0fc
ducafecat <ducafecat@gmail.com>
2020-04-20 05:38:49 +0800

    fix -> 感谢 GuaiKer 同学，提供 api 服务器 https://api.guaik.org

--- lib/common/values/server.dart
+++ lib/common/values/server.dart
@@ -1,1 +1,4 @@
-const SERVER_API_URL = 'http://yapi.demo.qunar.com/mock/88171';
+// const SERVER_API_URL = 'http://yapi.demo.qunar.com/mock/88171';
+
+// 感谢 GuaiKer 同学，提供 api 服务器 https://api.guaik.org
+const SERVER_API_URL = 'https://api.guaik.org/mock/74';
